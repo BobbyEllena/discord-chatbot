ca2869ff364b3c19d127e405d74ae06c444da973
Bobby Ellena <bobbyellena@gmail.com>
2021-11-02 22:18:56 -0500

    adding more fucntionality

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 const Discord = require("discord.js")
+const fetch = require("node-fetch")
 const client = new Discord.Client()
+
+function getQuote() {
+    return fetch("https://zenquotes.io/api/random")
+        .then(res => {
+            return res.json()
+        })
+        .then(data => {
+            return data[0]["q"] + " -" + data[0]["a"]
+        })
+}
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
 })
 
 client.on("message", msg => {
-    if (msg.content === "ping") {
-        MessagePort.reply("pong")
+    if (msg.autor.bot)  return 
+
+    if (msg.content === "Sinspire") {
+        getQuote().then(quote => msg.channel.send(quote))
     }
 })
 
